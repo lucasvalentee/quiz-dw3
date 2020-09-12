d6e99258783e7c74a511654a5f96ae39f3fbcc95
Lucas Valente <lucas.valente9999@gmail.com>
2020-09-12 17:38:15 -0300

    Alterado a estilização da tela inicial.

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 require_once('biblioteca/Autoload.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
   <?php include('view/layouts/head.html') ?>
-  <body class="text-center">
+  <body>
     <div class="container d-flex w-100 h-100 p-3 flex-column">
       <header class="mb-auto">Teste</header>
       <main role="main" class="d-flex w-100 h-100 inner flex-column justify-content-center">
-        <div class="w-50 h-50 align-self-center" style="background-color:red;">
-
+        <div id="d-main" class="w-50 align-self-center b-color-default">
+          <?php (isset($_SESSION['login']) && isset($_SESSION['senha'])) ? 'Você fez login!' : ControllerPadrao::getInstanceView('Login')->getHtml() ?>
         </div>
-        <?php echo (isset($_SESSION['login']) && isset($_SESSION['senha'])) ? 'Você fez login!' : ControllerPadrao::getInstanceView('Login')->getHtml() ?>
       </main>
       <?php include('view/layouts/footer.html') ?>
     </div>
--- view/ViewLogin.php
+++ view/ViewLogin.php
@@ -20,11 +20,22 @@ class ViewLogin {
   }
 
   private function getHtmlLogin() {
-    ob_start();
       ?>
-        <html></html>
+        
+        <form class="p-5 text-white">
+          <div class="form-group">
+            <label for="nome">Nome:&nbsp;</label>
+            <input type="text" class="form-control" id="nomeLogin" aria-describedby="nomeLogin">
+          </div>
+          <div class="form-group">
+            <label for="senha">Senha:&nbsp;</label>
+            <input type="password" class="form-control" id="senhaLogin">
+          </div>
+          <button type="submit" onclick="alert('Logou')" class="btn btn-success">Login</button>
+          <button type="submit" onclick="alert('Cadastrou ae')" class="btn btn-primary">Cadastre-se</button>
+        </form>
+
       <?php
-    
   }
 
 }
