751efb49427c7a85c8fd9aba564abeb99dac49a2
Kevin Marchi <kevin_marchi@hotmail.com>
2020-09-12 16:10:12 -0300

    Melhoria na estruturação

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
 require_once('biblioteca/Autoload.php');
+include('view/layouts/head.html');
+include('view/layouts/body.html');
+
+ControllerPadrao::getInstanceController('Login');
+
+include('view/layouts/footer.html')
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-  <?php include('view/layouts/head.html') ?>
-  <body class="text-center">
-    <div class="container d-flex w-100 h-100 p-3 flex-column">
-      
-      <?php echo (isset($_SESSION['login']) && isset($_SESSION['senha'])) ? 'Você fez login!' : ControllerPadrao::getInstanceView('Login')->getHtml() ?>
-    </div>
-  </body>
-  <?php include('view/layouts/footer.html') ?>
-</html>
