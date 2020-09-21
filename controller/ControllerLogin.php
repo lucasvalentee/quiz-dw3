7dcaa82e991d8e7ad221b47014bde1b94c92dd29
Kevin Marchi <kevin_marchi@hotmail.com>
2020-09-20 21:04:05 -0300

    Login básico

--- controller/ControllerLogin.php
+++ controller/ControllerLogin.php
@@ -1,14 +1,11 @@
 <?php
 require_once('../biblioteca/Autoload.php');
 
-$oControllerLogin = new ControllerLogin();
-
 class ControllerLogin {
 
     private $Conexao;
 
-    public function __construct() {
-        $this->Conexao = new Conexao();
+    public function __construct() {        
         $this->validaUserLogin();        
     }
 
@@ -17,20 +14,7 @@ class ControllerLogin {
     }
 
     private function validaUserLogin() {
-        $sLogin = $_POST['login'];
-        $sSenha = $_POST['senha'];
-        $oQuery = pg_query($this->Conexao->getConexao(), "SELECT * FROM tbusuario");
-        while ($aDados = pg_fetch_assoc($oQuery)) {
-            $bLogin = $aDados['usunome'];
-            $bSenha = $aDados['ususenha'];
-            if ($sLogin == $bLogin && $sSenha == $bSenha) {
-                $_SESSION['login'] = $sLogin;
-                $_SESSION['senha'] = $sSenha;                
-                echo json_encode('true');
-            } else {
-                echo json_encode('false');
-            }
-        }
+        
     }
 
 }
--- index.php
+++ index.php
@@ -8,12 +8,34 @@ session_start();
   <?php include('view/layouts/head.html') ?>
   <body>
     <div class="container d-flex w-100 h-100 p-3 flex-column">
-      <header class="mb-auto">Teste</header>
+      <header class="mb-auto"></header>
       <main role="main" class="d-flex w-100 h-100 inner flex-column justify-content-center">      
-        <div id="d-main" class="w-50 align-self-center b-color-default h-min-default">
-          <?php ControllerPadrao::getWindow() ?>
+        <div id="d-main" class="w-50 align-self-center b-color-default h-min-default">          
+          <?php            
+            validaLogin();            
+            ControllerPadrao::getWindow();
+          ?>
       </main>
       <?php include('view/layouts/footer.html') ?>      
     </div>
   </body>
 </html>
+
+<?php
+function validaLogin() {
+  if (isset($_POST['login']) && isset($_POST['senha'])) {
+    $oConexao = new Conexao();
+    echo 'teste';
+    $sLogin = $_POST['login'];
+    $sSenha = $_POST['senha'];
+    $oQuery = pg_query($oConexao->getConexao(), "SELECT * FROM tbusuario");
+    while ($aDados = pg_fetch_assoc($oQuery)) {
+        $bLogin = $aDados['usunome'];
+        $bSenha = $aDados['ususenha'];
+        if ($sLogin == $bLogin && $sSenha == $bSenha) {
+            $_SESSION['login'] = $sLogin;
+            $_SESSION['senha'] = $sSenha;                                       
+        }
+    }
+  }
+}
